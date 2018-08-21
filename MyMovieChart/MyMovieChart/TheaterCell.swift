ff401eb06dc0ae031d5c4662116a75e175ec88d2
byunsangjin <najombolre@gmail.com>
2018-08-21 21:57:09 +0900

    2018.8.21 21:57

---
+++ MyMovieChart/MyMovieChart/TheaterCell.swift
@@ -0,0 +1,14 @@
+//
+//  ㅅTheaterCell.swift
+//  MyMovieChart
+//
+//  Created by sjbyun on 2018. 8. 21..
+//  Copyright © 2018년 Byunsangjin. All rights reserved.
+//
+
+import UIKit
+class TheaterCell: UITableViewCell {
+    @IBOutlet var name: UILabel! // 상영관명
+    @IBOutlet var tel: UILabel! // 연락처
+    @IBOutlet var addr: UILabel! // 주소
+}
