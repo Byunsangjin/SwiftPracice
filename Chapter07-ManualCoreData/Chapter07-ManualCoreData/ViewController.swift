c47b02e99d5277002e65aad3706606d4d225682c
byunsangjin <qustkdwls1@naver.com>
2018-10-20 22:19:44 +0900

    CoreData를 이용한 데이터베이스 활용

---
+++ Chapter07-ManualCoreData/Chapter07-ManualCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chapter07-ManualCoreData
+//
+//  Created by Byunsangjin on 17/10/2018.
+//  Copyright © 2018 Byunsangjin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
